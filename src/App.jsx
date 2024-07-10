14737bb2af8fc8289b788e0b4b6372ca90d12487
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-10 10:53:33 -0500

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import Todo from './Components/Todo';
+import Footer from './Components/Footer';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Todo />
+      <React.Fragment>
+        {/* <header>
+          <h1 Home </h1>
+        </header> */}
+        <Todo />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
