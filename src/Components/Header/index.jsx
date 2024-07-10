0c3f5b069493526b57b398e1b9b56c2ca76df8a5
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-10 10:56:43 -0500

    Refactor header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+const Header = (props) => {
+  const { incomplete } = props;
+  
+  return (
+    <header>
+      <h1 data-testid="todo-h1">To Do List: {incomplete} items pending</h1>
+    </header>
+  )
+}
+
+export default Header;
