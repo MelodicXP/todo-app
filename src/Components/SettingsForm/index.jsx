9cea21092c09afb11bc058f3c8190d2c86b006ef
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-15 14:14:35 -0500

    Add Updated Settings card

--- src/Components/SettingsForm/index.jsx
+++ src/Components/SettingsForm/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Fieldset, TextInput, Switch, NumberInput } from '@mantine/core';
+import { Fieldset, TextInput, Switch, NumberInput, Grid, Card, Text, Group } from '@mantine/core';
 import { SettingsContext } from '../../Context/Settings';
 
 const SettingsForm = () => {
@@ -19,24 +19,48 @@ const SettingsForm = () => {
   };
 
   return (
-    <Fieldset legend="Update Settings">
-      <Switch
-        defaultChecked={hideCompleted}
-        label="Hide Completed ToDos"
-        onChange={handleSwitchChange}
-      />
+    <Grid
+      mt="md"
+      ml="md"
+    >
+      <Grid.Col span={4}>
+        <Fieldset legend="Update Settings">
+          <Switch
+            defaultChecked={hideCompleted}
+            label="Hide Completed ToDos"
+            onChange={handleSwitchChange}
+          />
 
-      <NumberInput
-        label="Items Per Page"
-        placeholder="Input Number"
-        value={displayItems}
-        min={1}
-        max={10}
-        onChange={handleNumberInputChange}
-      />
+          <NumberInput
+            label="Items Per Page"
+            placeholder="Input Number"
+            value={displayItems}
+            min={1}
+            max={10}
+            onChange={handleNumberInputChange}
+          />
 
-      <TextInput label="Sort Keyword" placeholder="difficulty" mt="md" />
-    </Fieldset>
+          <TextInput label="Sort Keyword" placeholder="difficulty" mt="md" />
+        </Fieldset>
+      </Grid.Col>
+
+      <Grid.Col span={4}>
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>Updated Settings</Text>
+        </Group>
+
+        <Text size="sm" >
+          <p>Hide Completed ToDos: {hideCompleted ? 'Yes' : 'No'}</p>
+          <p>Items Per Page: {displayItems}</p>
+        </Text>
+
+      </Card>
+      </Grid.Col>
+    </Grid>
+
+
   );
 };
 
