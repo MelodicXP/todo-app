b7cb29202e933c01fa3c932123e97bd102b8383d
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-15 14:50:09 -0500

    Render user settings when button clicked

--- src/Components/SettingsForm/index.jsx
+++ src/Components/SettingsForm/index.jsx
@@ -1,9 +1,10 @@
-import { useContext } from 'react';
-import { Fieldset, TextInput, Switch, NumberInput, Grid, Card, Text, Group } from '@mantine/core';
+import { useContext, useState } from 'react';
+import { Fieldset, TextInput, Switch, NumberInput, Grid, Card, Text, Group, Button } from '@mantine/core';
 import { SettingsContext } from '../../Context/Settings';
 
 const SettingsForm = () => {
   const { hideCompleted, setHideCompleted, displayItems, setDisplayItems } = useContext(SettingsContext);
+  const [settingsSubmitted, setSettingsSubmitted] = useState(false); // State to track settings submission
 
   // Handler for switching visibility of completed tasks
   const handleSwitchChange = (event) => {
@@ -18,13 +19,19 @@ const SettingsForm = () => {
     localStorage.setItem('displayItems', value); // Save to localStorage
   };
 
+  // Handler for showing updated settings
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSettingsSubmitted(true); // Set state to true when button is clicked
+  };
+
   return (
     <Grid
       mt="md"
       ml="md"
     >
       <Grid.Col span={4}>
-        <Fieldset legend="Update Settings">
+        <Fieldset component="form" legend="Update Settings" onSubmit={handleSubmit}> 
           <Switch
             defaultChecked={hideCompleted}
             label="Hide Completed ToDos"
@@ -41,23 +48,32 @@ const SettingsForm = () => {
           />
 
           <TextInput label="Sort Keyword" placeholder="difficulty" mt="md" />
+
+          <Button 
+            fullWidth 
+            mt="md" 
+            id='show-settings-button' 
+            type="submit" 
+            data-testid="show-settings-button"
+          >
+            Show New Settings
+          </Button>
         </Fieldset>
       </Grid.Col>
 
-      <Grid.Col span={4}>
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Updated Settings</Text>
-        </Group>
-
-        <Text size="sm" >
-          <p>Hide Completed ToDos: {hideCompleted ? 'Yes' : 'No'}</p>
-          <p>Items Per Page: {displayItems}</p>
-        </Text>
-
-      </Card>
-      </Grid.Col>
+      {settingsSubmitted && ( // Conditionally render the second Grid.Col
+        <Grid.Col span={4}>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>Updated Settings</Text>
+            </Group>
+            <Text size="sm">
+              <p>Hide Completed ToDos: {hideCompleted ? 'Yes' : 'No'}</p>
+              <p>Items Per Page: {displayItems}</p>
+            </Text>
+          </Card>
+        </Grid.Col>
+      )}
     </Grid>
 
 
