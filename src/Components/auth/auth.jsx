3de5b2af1375b82757bad788f4294765ced64a16
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-18 18:37:40 -0500

    Refactor to functional component

--- src/Components/auth/auth.jsx
+++ src/Components/auth/auth.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import {useContext} from 'react';
 import {When} from 'react-if';
-
 import { LoginContext } from './context.jsx';
 
-class Login extends React.Component {
+const Auth = ({ capability, children }) => {
+  // Use the useContext hook to access login context
+  const { loggedIn, can } = useContext(LoginContext);
+  
+  // Check if user logged in and has required capability
+  const isLoggedIn = loggedIn;
+  const canDo = capability ? can(capability) : true;
+  const okToRender = isLoggedIn && canDo;
 
-  static contextType = LoginContext;
+  // Render the children components if the condition is met
+  return (
+    <When condition={okToRender}>
+      {children}
+    </When>
+  );
+};
 
-  render() {
-
-    const isLoggedIn = this.context.loggedIn;
-    const canDo = this.props.capability ? this.context.can(this.props.capability) : true;
-    const okToRender = isLoggedIn && canDo;
-
-    return (
-      <When condition={okToRender}>
-        {this.props.children}
-      </When>
-    );
-  }
-}
-
-export default Login;
+export default Auth;
