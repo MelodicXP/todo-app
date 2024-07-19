92ca543dcadde02ad8d15ff74a9a9877b5fee1d8
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-19 11:18:08 -0500

    Update test incorporating auth

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,7 +1,7 @@
-// import React from 'react';
 import { describe, expect, it, afterEach } from 'vitest';
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import App from './App.jsx';
+import LoginProvider from './Components/auth/context.jsx'; // Import LoginProvider
 
 
 describe('App', () => {
@@ -10,10 +10,95 @@ describe('App', () => {
     localStorage.clear(); // Clear localStorage after each test
   });
 
-  it('should Add Item', () => {
+  const renderWithProviders = (ui, options) => {
+    return render(<LoginProvider>{ui}</LoginProvider>, options);
+  };
 
-    render(<App />);
+  it('should login and logout user', () => {
 
+    renderWithProviders(<App />);
+
+    // Assert login form fields render
+    const loginForm = screen.getByTestId('login-form');
+    const userNameLogin = screen.getByTestId('username-login');
+    const passwordLogin = screen.getByTestId('password-login');
+    const loginButton = screen.getByTestId('login-button');
+
+    // Assert login form appears on initial render
+    expect(loginForm).toBeInTheDocument();
+    expect(userNameLogin).toBeInTheDocument();
+    expect(passwordLogin).toBeInTheDocument();
+
+    // Mock user login credentials
+    let mockUserName = 'admin';
+    let mockPassword = 'ADMIN';
+
+    // Input values in login form
+    fireEvent.change(userNameLogin, {target: {value: mockUserName}});
+    fireEvent.change(passwordLogin, {target: {value: mockPassword}});
+
+    // Assert that the input values are updated correctly
+    expect(userNameLogin.value).toBe(mockUserName);
+    expect(passwordLogin.value).toBe(mockPassword);
+
+    // Fire login button
+    fireEvent.click(loginButton);
+    
+    // Assert logout button appears
+    let logoutButton = screen.getByTestId('logout-button');
+    expect(logoutButton).toBeInTheDocument();
+
+    // Fire logout button
+    fireEvent.click(logoutButton);
+
+    // Re-query the DOM for the login form fields after logging out
+    const loginFormAfterLogout = screen.getByTestId('login-form');
+    const userNameLoginAfterLogout = screen.getByTestId('username-login');
+    const passwordLoginAfterLogout = screen.getByTestId('password-login');
+
+    // Assert login form reappears after logging out
+    expect(loginFormAfterLogout).toBeInTheDocument();
+    expect(userNameLoginAfterLogout).toBeInTheDocument();
+    expect(passwordLoginAfterLogout).toBeInTheDocument()
+    
+  });
+
+  it('should login admin user and add item', () => {
+
+    renderWithProviders(<App />);
+
+    // Assert login form fields render
+    const loginForm = screen.getByTestId('login-form');
+    const userNameLogin = screen.getByTestId('username-login');
+    const passwordLogin = screen.getByTestId('password-login');
+    const loginButton = screen.getByTestId('login-button');
+
+    // Assert login form appears on initial render
+    expect(loginForm).toBeInTheDocument();
+    expect(userNameLogin).toBeInTheDocument();
+    expect(passwordLogin).toBeInTheDocument();
+
+    // Mock user login credentials
+    let mockUserName = 'admin';
+    let mockPassword = 'ADMIN';
+
+    // Input values in login form
+    fireEvent.change(userNameLogin, {target: {value: mockUserName}});
+    fireEvent.change(passwordLogin, {target: {value: mockPassword}});
+
+    // Assert that the input values are updated correctly
+    expect(userNameLogin.value).toBe(mockUserName);
+    expect(passwordLogin.value).toBe(mockPassword);
+
+    // Fire login button
+    fireEvent.click(loginButton);
+    
+    // Assert logout button appears
+    let logoutButton = screen.getByTestId('logout-button');
+    expect(logoutButton).toBeInTheDocument();
+
+
+    // Add items
     // Assert input user form renders
     const itemDetailsInput = screen.getByTestId('item-details-input');
     const assignedToInput = screen.getByTestId('assigned-to-input');
@@ -55,11 +140,58 @@ describe('App', () => {
 
     // Assert the checkbox is now checked
     expect(completeCheckbox).toBeChecked();
+
+    // Fire logout button
+    fireEvent.click(logoutButton);
+
+    // Re-query the DOM for the login form fields after logging out
+    const loginFormAfterLogout = screen.getByTestId('login-form');
+    const userNameLoginAfterLogout = screen.getByTestId('username-login');
+    const passwordLoginAfterLogout = screen.getByTestId('password-login');
+
+    // Assert login form reappears after logging out
+    expect(loginFormAfterLogout).toBeInTheDocument();
+    expect(userNameLoginAfterLogout).toBeInTheDocument();
+    expect(passwordLoginAfterLogout).toBeInTheDocument()
+    
   });
 
-  it('should delete an item', () => {
-    render(<App />);
+  it('should login admin user and delete item', () => {
 
+    renderWithProviders(<App />);
+
+    // Assert login form fields render
+    const loginForm = screen.getByTestId('login-form');
+    const userNameLogin = screen.getByTestId('username-login');
+    const passwordLogin = screen.getByTestId('password-login');
+    const loginButton = screen.getByTestId('login-button');
+
+    // Assert login form appears on initial render
+    expect(loginForm).toBeInTheDocument();
+    expect(userNameLogin).toBeInTheDocument();
+    expect(passwordLogin).toBeInTheDocument();
+
+    // Mock user login credentials
+    let mockUserName = 'admin';
+    let mockPassword = 'ADMIN';
+
+    // Input values in login form
+    fireEvent.change(userNameLogin, {target: {value: mockUserName}});
+    fireEvent.change(passwordLogin, {target: {value: mockPassword}});
+
+    // Assert that the input values are updated correctly
+    expect(userNameLogin.value).toBe(mockUserName);
+    expect(passwordLogin.value).toBe(mockPassword);
+
+    // Fire login button
+    fireEvent.click(loginButton);
+    
+    // Assert logout button appears
+    let logoutButton = screen.getByTestId('logout-button');
+    expect(logoutButton).toBeInTheDocument();
+
+
+    // Add items
     // Assert input user form renders
     const itemDetailsInput = screen.getByTestId('item-details-input');
     const assignedToInput = screen.getByTestId('assigned-to-input');
@@ -87,37 +219,19 @@ describe('App', () => {
 
     // Assert the list item is no longer present
     expect(listItem).not.toBeInTheDocument();
-  });
 
-  it('should mark item as complete', () => {
-    render(<App />);
+    // Fire logout button
+    fireEvent.click(logoutButton);
 
-    // Assert input user form renders
-    const itemDetailsInput = screen.getByTestId('item-details-input');
-    const assignedToInput = screen.getByTestId('assigned-to-input');
-    const addItemButton = screen.getByTestId('add-item-button');
+    // Re-query the DOM for the login form fields after logging out
+    const loginFormAfterLogout = screen.getByTestId('login-form');
+    const userNameLoginAfterLogout = screen.getByTestId('username-login');
+    const passwordLoginAfterLogout = screen.getByTestId('password-login');
 
-    // Mock user inputs
-    const mockTask = 'Task 1';
-    const mockAssignedTo = 'Person 1';
-
-    // Input values
-    fireEvent.change(itemDetailsInput, { target: { value: mockTask } });
-    fireEvent.change(assignedToInput, { target: { value: mockAssignedTo } });
-    // fireEvent.change(difficulty, { target: { value: mockDifficulty } });
-
-    // Fire add item button
-    fireEvent.click(addItemButton);
-
-    // Assert added list item appears on screen
-    let listItem = screen.getByTestId('list-item');
-    expect(listItem).toBeInTheDocument();
-
-    // Find and click the complete button
-    let completeCheckbox = screen.getByTestId('complete-checkbox');
-    fireEvent.click(completeCheckbox);
-
-    // Assert the list item is marked as complete
-    expect(completeCheckbox).toBeChecked();
+    // Assert login form reappears after logging out
+    expect(loginFormAfterLogout).toBeInTheDocument();
+    expect(userNameLoginAfterLogout).toBeInTheDocument();
+    expect(passwordLoginAfterLogout).toBeInTheDocument()
+    
   });
 });
